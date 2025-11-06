951c35c62ac8de89569c875818a4c078bd610ae5
Nelson Albera <nalbera@hotmail.com>
2025-11-06 17:43:57 -0300

    Creación de componente NavBar y modifico su UI agrego función de menú hamburguesa

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,14 +1,9 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import NavbarComponent from "../components/NavBarComponent";
 
 const RootLayout: React.FC = () => (
   <div className="min-h-screen flex flex-col">
-    <nav className="bg-[#C5D0EC] p-4 shadow-md flex flex-col sm:flex-row sm:justify-around sm:items-center">
-      <Link to="/" className="text-[#FDFD96] **font-lora** font-bold text-lg mb-2 sm:mb-0 hover:text-white transition duration-300">Inicio</Link>
-      <Link to="/login" className="text-[#FDFD96] **font-lora** font-bold text-lg mb-2 sm:mb-0 hover:text-white transition duration-300">Login</Link>
-      <Link to="/register" className="text-[#FDFD96] **font-lora** font-bold text-lg mb-2 sm:mb-0 hover:text-white transition duration-300">Registro</Link>
-      <Link to="/onboarding" className="text-[#FDFD96] **font-lora** font-bold text-lg mb-2 sm:mb-0 hover:text-white transition duration-300">Incorporación</Link>
-      <Link to="/dashboard" className="text-[#FDFD96] **font-lora** font-bold text-lg mb-2 sm:mb-0 hover:text-white transition duration-300">Dashboard</Link>
-    </nav>
+    <NavbarComponent />
     <main className="bg-[#F7F6F1] p-4 grow">
       <Outlet /> 
     </main>
